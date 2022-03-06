abf186158a66fd53ddccdc3a26cc6b8f4ac2c867
Jasan Hernández <jasan814@gmail.com>
2022-03-06 11:30:37 -0600

    [update] keyboard events

--- src/components/key.tsx
+++ src/components/key.tsx
@@ -1,14 +1,15 @@
 type Props = {
   letter?: string;
   width?: number | string
-  children?: JSX.Element | JSX.Element[]
+  children?: JSX.Element | JSX.Element[],
+  onClick?: () => void,
 }
 
-function Key({ letter, width, children }: Props) {
+function Key({ letter, width, children, onClick }: Props) {
   return (
-    <div style={{
+    <div onClick={onClick} style={{
       width: width || '45px',
-    }} className="text-lg py-3 px-4 border rounded-md cursor-pointer box-border flex justify-center bg-grayStrong dark:bg-darkGrayStrong active:bg-cyan-600">
+    }} className="text-lg py-3 px-4 rounded-md cursor-pointer box-border flex justify-center bg-grayStrong dark:bg-darkGrayStrong active:bg-cyan-600">
       {letter && <p className="select-none font-medium">{letter.toUpperCase()}</p>}
       {children}
     </div>
--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,23 +1,46 @@
 import Key from './key'
 import { FiDelete } from 'react-icons/fi'
+import { useContext } from 'react'
+import { GameContext } from '../context/gameContext'
 
-const rowOne = ['q', 'w', 'r', 't', 'y', 'u', 'i', 'o', 'p']
-const rowTwo = ['a','s','d','f','g','h','j','k','l','ñ']
-const rowThree = ['z','x','c','v','b','n','m']
+const rowOne = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
+const rowTwo = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ñ']
+const rowThree = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 
 function Keyboard() {
+  const { addLetterToCurrentInput, deleteWord, handleAddWord } = useContext(GameContext)
   return (
-    <div className='bg-lightGray dark:bg-darkLighGray py-8 flex gap-2 flex-wrap rounded-2xl'>
-      <div className='flex gap-2 pl-12'>
-        {rowOne.map((letter) => <Key key={letter} letter={letter} />)}
+    <div className="bg-lightGray dark:bg-darkLighGray py-8 flex gap-2 flex-wrap rounded-2xl">
+      <div className="flex gap-2 pl-12">
+        {rowOne.map((letter) => (
+          <Key
+            key={letter}
+            onClick={() => addLetterToCurrentInput(letter)}
+            letter={letter}
+          />
+        ))}
       </div>
-      <div className='flex gap-2 pl-16'>
-        {rowTwo.map((letter) => <Key key={letter} letter={letter} />)}
+      <div className="flex gap-2 pl-16">
+        {rowTwo.map((letter) => (
+          <Key
+            key={letter}
+            onClick={() => addLetterToCurrentInput(letter)}
+            letter={letter}
+          />
+        ))}
       </div>
-      <div className='flex gap-2 pl-5'>
-        <Key letter='enter' width={71} />
-        {rowThree.map((letter) => <Key key={letter} letter={letter} />)}
-        <Key width={71}><FiDelete size={26} /></Key>
+      <div className="flex gap-2 pl-5">
+        <Key letter="enter" width={71} onClick={handleAddWord} />
+        {rowThree.map((letter) => (
+          <Key
+            key={letter}
+            onClick={() => addLetterToCurrentInput(letter)}
+            letter={letter}
+          />
+        ))}
+        <Key width={71} onClick={deleteWord}>
+          <FiDelete size={26} />
+        </Key>
       </div>
     </div>
   )
